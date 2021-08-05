fc999c2cd06197aafe802560ee8bf715f9f714dc
AnkitPG <ankit@propertyguru.com.sg>
2021-08-05 10:50:37 +0800

    added route params

--- src/events.cotroller.ts
+++ src/events.cotroller.ts
@@ -1,18 +1,21 @@
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common'
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('/events')
 export class EventsController {
   @Get()
   findAll() {}
-  @Get()
-  findOne() {}
+
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return id
+  }
 
   @Post()
   create() {}
 
-  @Patch()
-  update() {}
+  @Patch(':id')
+  update(@Param('id') id) {}
 
-  @Delete()
-  remove() {}
+  @Delete(':id')
+  remove(@Param('id') id) {}
 }
