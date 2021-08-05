2e2316a68b0dbcb9e9c76aaee16a521d799228a7
AnkitPG <ankit@propertyguru.com.sg>
2021-08-05 10:36:49 +0800

    Added EventsController

---
+++ src/events.cotroller.ts
@@ -0,0 +1,18 @@
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common'
+
+@Controller('/events')
+export class EventsController {
+  @Get()
+  findAll() {}
+  @Get()
+  findOne() {}
+
+  @Post()
+  create() {}
+
+  @Patch()
+  update() {}
+
+  @Delete()
+  remove() {}
+}
