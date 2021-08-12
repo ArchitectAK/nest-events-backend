428480c9feb8523c58519901bed5e184beb68ed2
AnkitDroidGit <ankitdroiddeveloper@gmail.com>
2021-08-12 12:29:12 +0800

    Responses and status code

--- src/events.cotroller.ts
+++ src/events.cotroller.ts
@@ -3,15 +3,40 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
   Patch,
   Post,
-} from '@nestjs/common';
+} from '@nestjs/common'
 
 @Controller('/events')
 export class EventsController {
   @Get()
-  findAll() {}
+  findAll() {
+    return [
+      {
+        id: 1,
+        name: 'First Event',
+      },
+      {
+        id: 2,
+        name: 'First Event',
+      },
+      {
+        id: 3,
+        name: 'First Event',
+      },
+      {
+        id: 4,
+        name: 'First Event',
+      },
+      {
+        id: 5,
+        name: 'First Event',
+      },
+    ]
+  }
 
   @Get(':id')
   findOne(@Param('id') id) {
@@ -19,11 +44,16 @@ export class EventsController {
   }
 
   @Post()
-  create(@Body() input) {}
+  create(@Body() input) {
+    return input
+  }
 
   @Patch(':id')
-  update(@Param('id') id, @Body() input) {}
+  update(@Param('id') id, @Body() input) {
+    return input
+  }
 
   @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
   remove(@Param('id') id) {}
 }
